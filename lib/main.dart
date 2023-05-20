51c8dec833bc7caeff7fd9df0c623d77a962654f
oEduardoAfonso <oeduardoafonso@gmail.com>
2023-05-20 10:53:31 -0300

    Adicionando tema de cores

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:estacao_pilhas/pages/qr_code_reader/qr_code_reader.dart';
 import 'package:flutter/material.dart';
 
+import 'globals/colors.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -12,21 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Estação Pilhas',
-      theme: ThemeData(
-          colorScheme: ColorScheme.fromSwatch().copyWith(
-            primary: const Color.fromRGBO(3, 140, 62, 1),
-            onPrimary: const Color.fromRGBO(255, 255, 255, 1),
-            secondary: const Color.fromRGBO(199, 228, 211, 1),
-            background: const Color.fromRGBO(241, 241, 241, 1),
-            tertiary: const Color.fromRGBO(88, 122, 166, 1),
-            onSecondary: const Color.fromRGBO(0, 0, 0, 1),
-          ),
-          textTheme: const TextTheme(
-            titleMedium: TextStyle(fontFamily: 'Inter'),
-            titleLarge: TextStyle(fontFamily: 'Inter'),
-            titleSmall: TextStyle(fontFamily: 'Inter'),
-          ),
-          fontFamily: 'Roboto'),
+      theme: AppColors.appTheme,
       home: QrCodeReader(
         displayText: "Leia o Código QR localizado na parte X da máquina",
         onRead: (capture) {
